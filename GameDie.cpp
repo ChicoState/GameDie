ea8b4f0744bffd0293b0ca27a699d460fb8ee9e8
priyanka <pchordia@mail.csuchico.edu>
2016-01-29 10:55:53 -0800

    Roll Display Implementation

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
+using namespace std;
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+	srand(time(NULL));
 }
 
 //generate a random number between 1-6 (inclusive) and display it
 void GameDie::roll()
 {
+	cout << "Random number is: " << rand() % 6 + 1 << endl;
 }
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "GameDie.h"
+
+int main()
+{
+	GameDie game;
+	game.roll();
+	return 0;
+}
