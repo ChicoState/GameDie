a20421a0787479e21cbc207d1c76a357da32d3f6
joshiakshay91 <ajoshi6@mail.csuchico.edu>
2016-01-30 00:15:06 +0530

    done coding

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,14 +1,26 @@
 //I have successfully cloned and pushed
-
+#include <stdlib.h>
 #include <iostream>
 #include <cstdlib>
-
+#include <time.h>
+using std::cout;
+using std::string;
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+roll();
 }
 
 //generate a random number between 1-6 (inclusive) and display it
 void GameDie::roll()
 {
+
+int pivotIndex;
+std::default_random_engine kashi;
+std::random_device aj;
+std::uniform_int_distribution<int> rand(1,6);
+kashi.seed(aj());
+pivotIndex=rand(kashi);
+std::cout<<"This is the random number between 1 to 6 : " <<pivotIndex<<std::endl;
+
 }
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <random>
+#include "GameDie.h"
+//using namesspace std;
+
+int main ()
+{
+	GameDie g;
+	return 0;
+}
