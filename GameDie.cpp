5aae2edd6dda6c28310db094e6d83c7ca7368a3c
Akshay Jain <ajain2@mail.csuchico.edu>
2016-01-29 10:27:02 -0800

    Added code for generating random numbers

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <cstdlib>
+#include "GameDie.h"
+#include <random>
+using namespace std;
 
+std::default_random_engine generator;
 //class constructor that seeds the random number generator
+
+
 GameDie::GameDie()
 {
+  generator.seed(time(NULL));
 }
+
 
 //generate a random number between 1-6 (inclusive) and display it
 void GameDie::roll()
 {
+ int diceValue = 0;
+ std::uniform_int_distribution<int> distribution(1,6);
+ diceValue = distribution(generator);
+ cout<<"\nThe Dice Value is "<<diceValue;
 }
+
--- GameDie.h
+++ GameDie.h
@@ -1,12 +1,12 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
 
+
 class GameDie
 {
   public:
+   unsigned int time_ui;
    GameDie();
    void roll();
 };
-
-#include "GameDie.cpp"
 #endif
