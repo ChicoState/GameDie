7641c9f66b58849d7a794c808b03e1a4bd4b5995
Amit Shinde <amits83@gmail.com>
2016-01-29 10:43:50 -0800

    Changes to the GameDie program to make it work.
    -
    Amit

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <cstdlib>
+#include <time.h>
+#include "GameDie.h"
 
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+    srand(time(NULL));
 }
 
 //generate a random number between 1-6 (inclusive) and display it
-void GameDie::roll()
+int GameDie::roll()
 {
+    getnum = rand() % 6 + 1;
+    return getnum;
 }
--- GameDie.h
+++ GameDie.h
@@ -5,8 +5,10 @@ class GameDie
 {
   public:
    GameDie();
-   void roll();
+   int roll();
+ private :
+   int getnum;
 };
 
-#include "GameDie.cpp"
+//#include "GameDie.cpp"
 #endif
