171deb3d691de4cd2403869618b471017ea69266
[deoakshay] <[akshayd272@gmail.com]>
2016-01-29 13:18:20 -0800

    Rolled Display

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <cstdlib>
-
+#include <time.h>
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+ srand(time(NULL));
 }
 
 //generate a random number between 1-6 (inclusive) and display it
 void GameDie::roll()
 {
+ std::cout<<(rand()%6+1);
 }
