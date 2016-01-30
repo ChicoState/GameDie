03e73a2d2f962cec5dc77125d827addb49621bfb
praveendareddy21 <praveendareddy@gmail.com>
2016-01-29 22:48:09 -0800

    added roll method ot GameDie

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,5 +1,5 @@
 #include <cstdlib>
-
+#include <time.h>
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
@@ -8,4 +8,7 @@ GameDie::GameDie()
 //generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
+
+  srand (time(NULL));
+  return (rand() % 6) + 1;
 }
