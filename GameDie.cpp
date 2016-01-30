35f70ad9fcd90d3ba6c0fcb95e180ba3562b3f58
Satyajeet Swamy <satyajeetswamy@gmail.com>
2016-01-29 16:41:50 -0800

    Random Number Generation

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,12 +1,21 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include "GameDie.h"
+
+using namespace std;
 
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
 }
 
-//generate a random number between 1-6 (inclusive) and display it
-void GameDie::roll()
+//generate a random number between 1-6 (inclusive) and return it
+int GameDie::roll()
 {
+  srand(time(NULL));
+  int random_num = rand() % 6+1;
+  cout << "Random Number being generated: " << random_num << endl;
+
+  return random_num;
 }
--- GameDie.h
+++ GameDie.h
@@ -5,8 +5,8 @@ class GameDie
 {
   public:
    GameDie();
-   void roll();
+   int roll();
 };
 
-#include "GameDie.cpp"
+//#include "GameDie.cpp"
 #endif
