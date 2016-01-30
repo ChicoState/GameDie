d99100da2f0070ed3c403c81399ca22096995b42
Pratik Gundlupet Venkatesh <pgundlupetvenkatesh@mail.csuchico.edu>
2016-01-30 11:18:01 -0800

    My first checkin to GameDie

--- GameDie.cpp
+++ GameDie.cpp
@@ -3,13 +3,11 @@
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
-	srand(time(NULL));	//Initialize random seed.
+	
 }
 
 //generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
-	int rollDice;
-	rollDice = rand()%6+1;	//Genrate secret number between 1 and 6
-	printf("Number is: %i\n", rollDice);
+	
 }
