33e6c814915ba65110dae87bb0e549d762fbd350
rthakkar <rthakkar@mail.csuchico.edu>
2016-01-30 19:03:16 -0800

    Random Number Genrator

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,11 +1,21 @@
 #include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include "GameDie.h"
+using namespace std;
 
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+	srand(time(0));
 }
 
 //generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
+
+
+	int randNum = 1 + (rand()%6); 
+	return randNum;
 }
