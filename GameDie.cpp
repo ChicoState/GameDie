a7ac481119205a35ea23bac166ae41a029854212
Rohan Chordiya <rchordiya@mail.csuchico.edu>
2016-01-31 16:29:36 -0800

    Random number

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <cstdlib>
-
+#include <stdlib.h>
+#include <time.h>
+#include "GameDie.h"
+using namespace std;
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+   srand (time(NULL));
 }
 
 //generate a random number between 1-6 (inclusive) and display it
 void GameDie::roll()
 {
+   int r;
+   r = rand() % 6 + 1;
+   cout<<"Random number is "<<r<<endl;
 }
