3a910c7a3c4dc4a0695e766969a53397ce1dacd1
Diney Wankhede <dwankhede@mail.csuchico.edu>
2016-02-01 10:47:36 -0800

    seed random number generator

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,8 +1,12 @@
 #include <cstdlib>
+#include <iostream>
+#include <time.h>
 
+#include "GameDie.h"
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+   srand(time(NULL));
 }
 
 //generate a random number between 1-6 (inclusive) and return it
