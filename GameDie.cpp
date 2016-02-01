29dd8b1cebf66570d6f89447fe6fc3138b1b9dca
Diney Wankhede <dwankhede@mail.csuchico.edu>
2016-02-01 12:31:38 -0800

    Change return type of roll function

--- GameDie.cpp
+++ GameDie.cpp
@@ -10,7 +10,7 @@ GameDie::GameDie()
 }
 
 //generate a random number between 1-6 (inclusive) and return it
-int GameDie::roll()
+void GameDie::roll()
 {
    int randomNumber = rand() % 6 + 1;
    std::cout << randomNumber << std::endl;
