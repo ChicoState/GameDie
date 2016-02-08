ce0e5f1a4185437027021e3d93cf18e3a9ee13f2
Amey Deshpande <ameyadeshpande90@gmail.com>
2016-02-08 10:36:37 -0800

    Die has sides from 4 to 20

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <time.h>
 #include "GameDie.h"
+#include <iostream>
 
 using namespace std;
 
@@ -13,6 +14,9 @@ GameDie::GameDie()
 //generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
-	int i=(rand()%6) + 1;
+  cout<<"Enter number of sides greater than or equal to 4 and less than 21"<<endl;
+  int sides;
+  cin>>sides;
+	int i=(rand()%sides) + 1;
 	return i;
 }
