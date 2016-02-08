5a4fc8deda4dafe32a79098b5c6784225b13a8ff
Amit Shinde <amits83@gmail.com>
2016-02-08 10:20:22 -0800

    Making change for 4 to 20 sides

--- GameDie.cpp
+++ GameDie.cpp
@@ -7,11 +7,12 @@
 GameDie::GameDie()
 {
     srand(time(NULL));
+    getnum = rand() % 20 + 4;
 }
 
 //generate a random number between 1-6 (inclusive) and display it
 int GameDie::roll()
 {
-    getnum = rand() % 6 + 1;
-    return getnum;
+    getroll = rand() % getnum + 1;
+    return getroll;
 }
--- GameDie.h
+++ GameDie.h
@@ -8,6 +8,7 @@ class GameDie
    int roll();
  private :
    int getnum;
+   int getroll;
 };
 
 //#include "GameDie.cpp"
