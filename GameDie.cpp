1204e7f76ccfb8e424f49a69e97f5274c4c76865
priyankavrao97 <prao1@mail.csuchico.edu>
2023-02-07 09:56:48 -0800

    Fixed 2 issues get_distribution and alternate-sized die

--- GameDie.cpp
+++ GameDie.cpp
@@ -10,10 +10,35 @@ GameDie::GameDie()
       counter[i] = 0;
 }
 
+GameDie::GameDie(unsigned int faces){
+    if(faces==0)
+        SIDES=6;
+    else
+        SIDES=faces;
+
+}
+
 //generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
     int roll = rand() % SIDES;
     counter[roll]++;
+    //get_distribution(roll+1);
     return roll + 1;
+}
+
+
+vector<int> GameDie::get_distribution(int roll)
+{
+    vector<int> result;
+    for(int i=0;i<SIDES;i++){
+        if(i==roll-1){
+            result[i]=1;
+        }
+        else{
+            result[i]=0;
+        }
+    }
+    return result;
+
 }
--- GameDie.h
+++ GameDie.h
@@ -1,11 +1,14 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
 
+
 class GameDie
 {
   public:
    GameDie();
    int roll();
+   vector<int> get_distribution(int);
+   GameDie(unsigned int);
   private:
    const static int SIDES = 6;
    int counter[SIDES];
