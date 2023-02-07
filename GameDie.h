b21c834308498d171a1f9c1d86e00a7bf0e47b49
Sathya <svvsathyanarayanan@gmail.com>
2023-02-07 09:58:12 -0800

    add : get distribution method to GameDie class

--- GameDie.h
+++ GameDie.h
@@ -1,14 +1,18 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
-
+#include <vector>
+using namespace std;
 class GameDie
 {
-  public:
-   GameDie();
-   int roll();
-  private:
-   const static int SIDES = 6;
-   int counter[SIDES];
+public:
+  GameDie();
+  int roll();
+  vector<int> get_distribution();
+
+private:
+  const static int SIDES = 6;
+  int counter[SIDES];
+  vector<int> distribution;
 };
 
 #endif
