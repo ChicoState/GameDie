d114d21df9e3c32e23d93eb34a3c41b437357016
Yashvi <ysiddhapura@mail.csuchico.edu>
2023-02-07 10:09:26 -0800

    Resolves #43, #44 Added member func get_distribution and altr die size construc

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,10 +1,29 @@
 #include "GameDie.h"
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+
+using namespace std;
+
+
+GameDie::GameDie(unsigned int a)
+{
+    if(a != 0){
+        SIDES= a;
+        srand(time(NULL));
+        for(int i=0; i<SIDES; i++)
+        counter[i] = 0;
+    }
+    else{
+        SIDES = 6;
+    }
+    
+}
 
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+    SIDES = 6;
     srand(time(NULL));
     for(int i=0; i<SIDES; i++)
       counter[i] = 0;
@@ -13,7 +32,24 @@ GameDie::GameDie()
 //generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
+
     int roll = rand() % SIDES;
     counter[roll]++;
     return roll + 1;
+}
+
+vector<int> GameDie::get_distribution(){
+    vector<int> distr;
+    
+    for(int i = 0; i<SIDES; i++){
+        distr.push_back(0);
+    }
+    int rolls = roll();
+    for(int i = 0; i<SIDES; i++){
+        
+        distr[rolls-1] +=1;
+    }
+
+    return distr;
+
 }
--- GameDie.h
+++ GameDie.h
@@ -1,14 +1,19 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
 
+
 class GameDie
 {
   public:
    GameDie();
+   GameDie(unsigned int a);
    int roll();
+   vector<int> get_distribution();
+  
   private:
-   const static int SIDES = 6;
-   int counter[SIDES];
+   int SIDES;
+   int counter[6];
+
 };
 
 #endif
