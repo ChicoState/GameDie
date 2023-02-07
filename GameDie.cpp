e6ac9f7cbd4ec4158dfd2a5970daa4b8957d8442
Sathya <svvsathyanarayanan@gmail.com>
2023-02-07 09:58:35 -0800

    implement : get_distribution method

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,19 +1,30 @@
 #include "GameDie.h"
 #include <cstdlib>
 #include <ctime>
-
-//class constructor that seeds the random number generator
+#include <iostream>
+#include <vector>
+using namespace std;
+// class constructor that seeds the random number generator
 GameDie::GameDie()
 {
     srand(time(NULL));
-    for(int i=0; i<SIDES; i++)
-      counter[i] = 0;
+    for (int i = 0; i < SIDES; i++)
+    {
+        counter[i] = 0;
+        distribution.push_back(0);
+    }
 }
 
-//generate a random number between 1-6 (inclusive) and return it
+// generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
     int roll = rand() % SIDES;
     counter[roll]++;
+    distribution[roll]++;
     return roll + 1;
+}
+
+vector<int> GameDie::get_distribution()
+{
+    return distribution;
 }
