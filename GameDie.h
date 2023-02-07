23c5cd061bfe4343609ddc7c3849ddc5450785db
Sathya <svvsathyanarayanan@gmail.com>
2023-02-07 10:22:26 -0800

    add : overloaded constructor for creating a new game die

--- GameDie.h
+++ GameDie.h
@@ -1,14 +1,18 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
-
+#include <vector>
+using namespace std;
 class GameDie
 {
-  public:
-   GameDie();
-   int roll();
-  private:
-   const static int SIDES = 6;
-   int counter[SIDES];
+public:
+  GameDie();
+  GameDie(unsigned custom_faces);
+  int roll();
+  vector<int> get_distribution();
+
+private:
+  const static int SIDES = 6;
+  vector<unsigned> counter;
 };
 
 #endif
