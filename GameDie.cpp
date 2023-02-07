be44dc97c32f6bc580ed0d6e0ad099041bad9644
hamsaraj <hamsaraj.tupthi18@gmail.com>
2023-02-07 10:25:57 -0800

    resolved the issues

--- GameDie.cpp
+++ GameDie.cpp
@@ -6,14 +6,38 @@
 GameDie::GameDie()
 {
     srand(time(NULL));
-    for(int i=0; i<FACES; i++)
+    counter.resize(6);
+    for(int i=0; i<SIDES; i++)
       counter[i] = 0;
 }
 
+GameDie::GameDie(unsigned faces)
+{
+    srand(time(NULL));
+    counter.resize(6);
+    if(faces!=0)
+    {
+        SIDES=faces;
+        counter.resize(faces);
+    }
+    for(int i=0; i<SIDES; i++)
+      counter[i] = 0;
+}
 //generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
-    int roll = rand() % FACES;
+    int roll = rand() % SIDES;
     counter[roll]++;
     return roll + 1;
+}
+
+//returns the the number of rolls for each face of the die.
+vector<int> GameDie::get_distribution()
+{
+    vector<int> distribution(SIDES);
+    for(int i=0;i<SIDES;i++)
+    {
+        distribution[i]=counter[i];
+    }
+    return distribution;
 }
--- GameDie.h
+++ GameDie.h
@@ -1,14 +1,18 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
+#include <vector>
+using namespace std;
 
 class GameDie
 {
   public:
    GameDie();
+   GameDie(unsigned faces);
    int roll();
+   vector<int> get_distribution();
   private:
-   const static int FACES = 6;
-   int counter[FACES];
+   int SIDES = 6;
+   vector<int> counter;
 };
 
 #endif
