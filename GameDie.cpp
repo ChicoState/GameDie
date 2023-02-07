2433cc58e74bd49a90632d1922cf84fca9e5d65a
shalavritha mamunooru <shalavritha.srtforever@gmail.com>
2023-02-07 10:20:18 -0800

    function added get_distribution()

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,6 +1,9 @@
 #include "GameDie.h"
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+using namespace std; // change 1 - adding this namespace
+vector<int>Die_count; // change 1 - vector has been created for get_distribution function
 
 //class constructor that seeds the random number generator
 GameDie::GameDie()
@@ -8,6 +11,10 @@ GameDie::GameDie()
     srand(time(NULL));
     for(int i=0; i<FACES; i++)
       counter[i] = 0;
+      //initializing the vector elements to zeros initially
+      for (int i = 0; i < Die_count.size()-1; i++)
+        Die_count[i]=0;
+      
 }
 
 //generate a random number between 1-6 (inclusive) and return it
@@ -16,4 +23,13 @@ int GameDie::roll()
     int roll = rand() % FACES;
     counter[roll]++;
     return roll + 1;
+}
+
+//change 1 - public member function that returns the count of a face in the die
+vector<int>GameDie::get_distribution()
+{
+    int die_num= roll();
+    Die_count[die_num-1]=+1;
+    return Die_count;
+    
 }
--- GameDie.h
+++ GameDie.h
@@ -1,11 +1,13 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
-
+#include <vector> // change 1 - including #include<vector>
+using namespace std; // change 1 - including std
 class GameDie
 {
   public:
    GameDie();
    int roll();
+   vector<int>get_distribution(); // change 1 - created a public function as named to return a vector
   private:
    const static int FACES = 6;
    int counter[FACES];
