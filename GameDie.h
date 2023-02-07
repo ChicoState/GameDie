0989cb4e9d4e934fab9f89381e328ea627b39701
Chaitanya <45332082+chetan2298@users.noreply.github.com>
2023-02-07 10:26:09 -0800

    Update GameDie.h

--- GameDie.h
+++ GameDie.h
@@ -1,14 +1,17 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
-
+#include <vector>
+using std::vector;
 class GameDie
 {
-  public:
-   GameDie();
-   int roll();
-  private:
-   const static int FACES = 6;
-   int counter[FACES];
+public:
+  GameDie();
+  int roll();
+  std::vector<int> get_distribution();
+
+private:
+  const static int FACES = 6;
+  int counter[FACES];
 };
 
 #endif
