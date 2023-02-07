c18d9b2b2ce60bba3696b12d2769ceb40f9b4264
Anoushka Sharma <asharma7@csuchico.edu>
2023-02-07 09:56:15 -0800

    Overloaded constructor for alternate sized die, issue 43 resolved!

--- GameDie.cpp
+++ GameDie.cpp
@@ -2,15 +2,20 @@
 #include <cstdlib>
 #include <ctime>
 
-//class constructor that seeds the random number generator
+// class constructor that seeds the random number generator
 GameDie::GameDie()
 {
     srand(time(NULL));
-    for(int i=0; i<SIDES; i++)
-      counter[i] = 0;
+    for (int i = 0; i < SIDES; i++)
+        counter[i] = 0;
 }
 
-//generate a random number between 1-6 (inclusive) and return it
+GameDie::GameDie(unsigned a)
+{
+    for (int i = 0; i < a; i++)
+        counter[i] = 0;
+}
+// generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
     int roll = rand() % SIDES;
--- GameDie.h
+++ GameDie.h
@@ -3,12 +3,14 @@
 
 class GameDie
 {
-  public:
-   GameDie();
-   int roll();
-  private:
-   const static int SIDES = 6;
-   int counter[SIDES];
+public:
+  GameDie(); // default constructor
+  int roll();
+  GameDie(unsigned int); // overloaded constructor for issue 43
+
+private:
+  const static int SIDES = 6;
+  int counter[SIDES];
 };
 
 #endif
