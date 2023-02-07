621b45bc5e366acebfc548528b48f629b6652cfe
V.Prasanna <30752161+prasannarajezzzy@users.noreply.github.com>
2023-02-07 10:23:30 -0800

    Issue create a member function

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,19 +1,26 @@
 #include "GameDie.h"
 #include <cstdlib>
 #include <ctime>
-
-//class constructor that seeds the random number generator
+#include <vector>
+#include <iostream>
+using namespace std;
+// class constructor that seeds the random number generator
 GameDie::GameDie()
 {
     srand(time(NULL));
-    for(int i=0; i<SIDES; i++)
-      counter[i] = 0;
+    for (int i = 0; i < SIDES; i++)
+        counter[i] = 0;
 }
 
-//generate a random number between 1-6 (inclusive) and return it
+// generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
     int roll = rand() % SIDES;
     counter[roll]++;
+    results.push_back(roll + 1);
     return roll + 1;
+}
+vector<int> GameDie::get_distribution()
+{
+    return results;
 }
