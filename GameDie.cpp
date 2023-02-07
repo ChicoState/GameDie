243864fe0d2b4eecc513c9696cdb112ba33c1b5c
Rahul Belwal <belwalrahul@gmail.com>
2023-02-07 10:07:04 -0800

    Changes added for issue 1: get_distribution()

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,6 +1,9 @@
 #include "GameDie.h"
 #include <cstdlib>
+#include <iostream>
 #include <ctime>
+
+using namespace std;
 
 //class constructor that seeds the random number generator
 GameDie::GameDie()
@@ -16,4 +19,12 @@ int GameDie::roll()
     int roll = rand() % SIDES;
     counter[roll]++;
     return roll + 1;
+}
+
+vector<int> GameDie::get_distribution()
+{
+    int n = sizeof(counter) / sizeof(counter[0]);
+    vector<int> dist(counter, counter + n);
+    
+    return dist;
 }
--- GameDie.h
+++ GameDie.h
@@ -1,11 +1,15 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
 
+#include <vector>
+using namespace std;
+
 class GameDie
 {
   public:
    GameDie();
    int roll();
+   vector<int> get_distribution();
   private:
    const static int SIDES = 6;
    int counter[SIDES];
