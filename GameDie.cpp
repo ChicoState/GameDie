4427360adb921f524b668f16ed4ef8cd105eb628
Preston Massey-Blake <pmassey-blake@mail.csuchico.edu>
2023-02-07 10:33:43 -0800

    Closes #43, closes #44; added gameDie functionality

--- GameDie.cpp
+++ GameDie.cpp
@@ -5,15 +5,37 @@
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+   
     srand(time(NULL));
     for(int i=0; i<SIDES; i++)
-      counter[i] = 0;
+    {
+    disCounter.push_back(0);
+    }
+   
 }
+// constructor that the die can be created with any positive number of faces
+    GameDie::GameDie(unsigned int val){
+        if (val == 0)
+        {
+            GameDie();
+        }
+        else if (val < 0)
+        {
+            std::cout << "number of sides must be 0 or greater" << std::endl;
+        }
+        else if (val > 0)
+        {
+            for (int i = 0; i < val; i++)
+            {
+                disCounter.push_back(0);
+            }
+        }
+    }
 
-//generate a random number between 1-6 (inclusive) and return it
-int GameDie::roll()
+    //generate a random number between 1-6 (inclusive) and return it
+    int GameDie::roll()
 {
-    int roll = rand() % SIDES;
-    counter[roll]++;
+    int roll = rand() % disCounter.size() - 1;
+    disCounter[roll]++;
     return roll + 1;
 }
--- GameDie.h
+++ GameDie.h
@@ -1,14 +1,17 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
-
+#include <vector>
+#include <iostream>
 class GameDie
 {
   public:
    GameDie();
+   GameDie(unsigned int val);
    int roll();
   private:
    const static int SIDES = 6;
    int counter[SIDES];
+   std::vector<int> disCounter;
 };
 
 #endif
