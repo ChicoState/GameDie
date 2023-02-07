52e2d6f75ab7cc26d7e46f98f446143a0a22654d
Sathya <svvsathyanarayanan@gmail.com>
2023-02-07 10:24:52 -0800

    implement : overload constructor for game die

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,19 +1,45 @@
 #include "GameDie.h"
 #include <cstdlib>
 #include <ctime>
-
-//class constructor that seeds the random number generator
+#include <iostream>
+#include <vector>
+using namespace std;
+// class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+
     srand(time(NULL));
-    for(int i=0; i<SIDES; i++)
-      counter[i] = 0;
+    for (int i = 0; i < SIDES; i++)
+    {
+        counter.push_back(0);
+    }
 }
 
-//generate a random number between 1-6 (inclusive) and return it
+GameDie::GameDie(unsigned custom_faces)
+{
+    unsigned faces = custom_faces == 0 ? SIDES : custom_faces;
+
+    srand(time(NULL));
+    for (int i = 0; i < faces; i++)
+    {
+        counter.push_back(0);
+    }
+}
+
+// generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
     int roll = rand() % SIDES;
     counter[roll]++;
     return roll + 1;
+}
+
+vector<int> GameDie::get_distribution()
+{
+    vector<int> distribution;
+    for (unsigned i = 0; i < counter.size(); i++)
+    {
+        distribution.push_back(counter[i]);
+    }
+    return distribution;
 }
