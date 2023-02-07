82965504976d193d01313cdb14ec84cc2bff67f0
Chaitanya <45332082+chetan2298@users.noreply.github.com>
2023-02-07 10:25:44 -0800

    Updated Gamedie.cpp with getdistribution()

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,19 +1,39 @@
 #include "GameDie.h"
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+using std::vector;
+#include <iostream>
+using namespace std;
 
-//class constructor that seeds the random number generator
+// class constructor that seeds the random number generator
 GameDie::GameDie()
 {
     srand(time(NULL));
-    for(int i=0; i<FACES; i++)
-      counter[i] = 0;
+    for (int i = 0; i < FACES; i++)
+        counter[i] = 0;
 }
 
-//generate a random number between 1-6 (inclusive) and return it
+// generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
     int roll = rand() % FACES;
     counter[roll]++;
     return roll + 1;
 }
+
+std::vector<int> GameDie::get_distribution()
+{
+    std::vector<int> x;
+    for (auto i : counter)
+        x.push_back(i);
+    return x;
+}
+
+// int main()
+// {
+//     GameDie x;
+//     vector<int> z = x.get_distribution();
+//     for (auto i : z)
+//         cout << i;
+// }
