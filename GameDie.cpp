bfe743048b75b26dad2ea106cbb894adc0f3e51e
David Coles <davidepcoles@gmail.com>
2023-02-07 10:01:22 -0800

    added overloaded operator to Gamedie.cpp resolves #43

--- GameDie.cpp
+++ GameDie.cpp
@@ -10,6 +10,15 @@ GameDie::GameDie()
       counter[i] = 0;
 }
 
+//Overload constructor with a dice of any number of faces
+GameDie::GameDie(unsigned int val)
+{
+    srand(time(NULL));
+    FACES = val;
+    for(int i=0; i<FACES; i++)
+      counter[i] = 0;
+}
+
 //generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
