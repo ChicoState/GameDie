fa4149387a5b479727ee5fe32b6dc8390cbc18a9
Manohar Mendan <mendan.m@gmail.com>
2023-02-07 10:15:48 -0800

    Created a member function & an overloaded constructor

--- GameDie.cpp
+++ GameDie.cpp
@@ -2,18 +2,35 @@
 #include <cstdlib>
 #include <ctime>
 
-//class constructor that seeds the random number generator
-GameDie::GameDie()
+//default class constructor that seeds the random number generator
+GameDie::GameDie() : FACES(6)
 {
-    srand(time(NULL));
-    for(int i=0; i<FACES; i++)
-      counter[i] = 0;
+ srand(time(NULL));
+ counter.resize(FACES, 0);
 }
 
-//generate a random number between 1-6 (inclusive) and return it
+//overloaded class constructor
+GameDie::GameDie(unsigned int faces)
+{
+ if(faces == 0)
+ FACES = 6;
+ else
+ FACES = faces;
+ srand(time(NULL));
+ counter.resize(FACES, 0);
+}
+
+//generate a random number between 1-FACES (inclusive) and return it
 int GameDie::roll()
 {
-    int roll = rand() % FACES;
-    counter[roll]++;
-    return roll + 1;
+ int roll = rand() % FACES;
+ counter[roll]++;
+ return roll + 1;
 }
+
+//public member function that returns a vector representing the number of rolls for each face of the die
+std::vector<int> GameDie::get_distribution()
+{
+ return counter;
+}
+
--- GameDie.h
+++ GameDie.h
@@ -1,14 +1,18 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
 
+#include <vector>
+
 class GameDie
 {
-  public:
-   GameDie();
-   int roll();
-  private:
-   const static int FACES = 6;
-   int counter[FACES];
+public:
+ GameDie();
+ GameDie(unsigned int faces);
+ int roll();
+ std::vector<int> get_distribution();
+private:
+ unsigned int FACES;
+ std::vector<int> counter;
 };
 
 #endif
