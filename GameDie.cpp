3fada7e74bbc9cb168097c926ffc338f82ce1266
SANDESHSOBARAD <sobaradsandesh@gmail.com>
2023-02-07 10:01:00 -0800

    resolved the issues raised in GameDie

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,7 +1,9 @@
 #include "GameDie.h"
 #include <cstdlib>
 #include <ctime>
-
+#include <vector>
+#include <algorithm>
+#include <numeric>
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
@@ -17,3 +19,27 @@ int GameDie::roll()
     counter[roll]++;
     return roll + 1;
 }
+
+std::vector<double> GameDie::get_distribution() const
+{
+  std::vector<double> distribution(SIDES);
+  int total_rolls = std::accumulate(counter, counter + SIDES, 0);
+  for (int i = 0; i < SIDES; i++)
+  {
+    distribution[i] = static_cast<double>(counter[i]) / total_rolls;
+  }
+  return distribution;
+}
+
+
+GameDie::GameDie(unsigned int faces)
+{
+  if (faces == 0)
+  {
+    faces = 6;
+  }
+  SIDES = faces;
+  std::fill(counter, counter + SIDES, 0);
+  distribution.resize(SIDES);
+}
+
--- GameDie.h
+++ GameDie.h
@@ -1,11 +1,14 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
-
+#include <vector>
 class GameDie
 {
   public:
    GameDie();
    int roll();
+   vector<double> get_distribution()
+   GameDie(unsigned int faces)
+
   private:
    const static int SIDES = 6;
    int counter[SIDES];
