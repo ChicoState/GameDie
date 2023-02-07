4ab4f18cbe498252f54848b23dc6ad04aa2ffdbb
Saurabh Kulkarni <srbhkulkarni13@gmail.com>
2023-02-07 18:07:04 +0000

    Added a constructor for any die size fixes Overload constructor for alternate-sized die #43

--- GameDie.cpp
+++ GameDie.cpp
@@ -5,15 +5,24 @@
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+    currentFaces = 6;
     srand(time(NULL));
-    for(int i=0; i<FACES; i++)
-      counter[i] = 0;
+    for(int i=0; i<currentFaces; i++)
+      counter.push_back(0);
+}
+
+GameDie::GameDie(unsigned int cf) {
+    srand(time(NULL));
+    currentFaces = cf;
+    srand(time(NULL));
+    for(int i=0; i<currentFaces; i++)
+      counter.push_back(0);
 }
 
 //generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
-    int roll = rand() % FACES;
+    int roll = rand() % currentFaces;
     counter[roll]++;
     return roll + 1;
 }
--- GameDie.h
+++ GameDie.h
@@ -1,14 +1,17 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
 
+#include <vector>
+
 class GameDie
 {
   public:
    GameDie();
+   GameDie(unsigned int);
    int roll();
   private:
-   const static int FACES = 6;
-   int counter[FACES];
+   unsigned int currentFaces;
+   std::vector<int> counter;
 };
 
 #endif
