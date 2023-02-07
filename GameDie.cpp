236901b7206b9185263b1ee1ae333dcd935a2adf
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-02-07 10:16:31 -0800

    closes #43- added overloaded consturctor for alternate-sized die

--- GameDie.cpp
+++ GameDie.cpp
@@ -2,18 +2,35 @@
 #include <cstdlib>
 #include <ctime>
 
-//class constructor that seeds the random number generator
+// class constructor that seeds the random number generator
 GameDie::GameDie()
 {
     srand(time(NULL));
-    for(int i=0; i<FACES; i++)
-      counter[i] = 0;
+    for (int i = 0; i < FACES; i++)
+        counter[i] = 0;
 }
 
-//generate a random number between 1-6 (inclusive) and return it
+// overloaded class constructor that seeds the random number generator for custom no. of faces
+GameDie::GameDie(int n)
+{
+    custom_FACES = n;
+    cf_enabled = true;
+    srand(time(NULL));
+    for (int i = 0; i < custom_FACES; i++)
+        counter[i] = 0;
+}
+// generate a random number between 1-6 (inclusive) or 1-custom_Faces and return it
 int GameDie::roll()
 {
-    int roll = rand() % FACES;
+    int roll;
+    if (cf_enabled)
+    {
+        roll = rand() % custom_FACES;
+    }
+    else
+    {
+        roll = rand() % FACES;
+    }
     counter[roll]++;
     return roll + 1;
 }
--- GameDie.h
+++ GameDie.h
@@ -3,12 +3,16 @@
 
 class GameDie
 {
-  public:
-   GameDie();
-   int roll();
-  private:
-   const static int FACES = 6;
-   int counter[FACES];
+public:
+  GameDie();
+  GameDie(int n);
+  int roll();
+
+private:
+  const static int FACES = 6;
+  int custom_FACES;
+  bool cf_enabled = false;
+  int counter[FACES];
 };
 
 #endif
