91b3d56bf730a93f08538cb72609731f80b05111
aalfaro5 <98926113+aalfaro5@users.noreply.github.com>
2023-02-20 11:40:57 -0800

    Update GameDie.cpp
    implement percentage function

--- GameDie.cpp
+++ GameDie.cpp
@@ -44,3 +44,26 @@ int GameDie::roll()
 vector <int> GameDie::get_distribution(){
     return counter;
 }
+vector<double> GameDie::get_percentages(){
+    int rolls = 0;
+    vector<double> pVector;
+    for (int i = 0; i < (int)counter.size(); i++)
+    {
+        rolls += counter[i];
+    }
+    //check if rolls is 0, fill vector with 0s if true
+    if( rolls == 0)
+    {
+        for(int i = 0; i < FACES; i++){
+        pVector.push_back(0);}
+    }//otherwise get percentage and pushback onto pVector
+    else
+    {
+        for(int i = 0; i < FACES; i++)
+        {
+            pVector.push_back((double)counter[i] / (double)rolls);
+        }
+    }
+    
+    return pVector;
+}
